82443c2879349fdc6292a246a7590c91f22f96a5
Taimiya Tahsin <taimiyatahsin0@gmail.com>
2026-09-24 12:49:40 +0600

    Add footer year and temporary link message

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+// Current year in footer
+document.getElementById("year").textContent = new Date().getFullYear();
 
+
+// Temporary message for links that haven't been added yet
+function showMessage(event) {
+    event.preventDefault();
+
+    alert(
+        "This link will be added soon. You can update it with your LinkedIn profile or email."
+    );
+}
